8410198e74a5f64aeb705da465d43b6be583f32e
diamant1991 <diamant1988@mail.ru>
2018-02-26 14:49:52 +0200

    adaptive main page

--- js/js.js
+++ js/js.js
@@ -16,10 +16,32 @@ $('.main-slider').slick({
   fade: true,
   cssEase: 'linear'
 });
-$('#clock').countdown('2018/02/28', function(event) {
+$('#clock').countdown('2018/03/05', function(event) {
   var $this = $(this).html(event.strftime(''
     + '<div class="countdown-item"><div class="countdown-title">%d</div> <div class="countdown-name">дня</div></div> '
     + '<div class="countdown-item"><div class="countdown-title">%H</div> <div class="countdown-name">часов</div></div> '
     + '<div class="countdown-item"><div class="countdown-title">%M</div> <div class="countdown-name">минут</div></div> '
     + '<div class="countdown-item"><div class="countdown-title">%S</div> <div class="countdown-name">секунд</div></div>'));
+});
+
+$('.hamburger-box').click(function(e) {
+  var dropdown = $('.mobile-panel');
+  if(dropdown.is(':hidden')){
+    $(this).addClass('is-active')
+    dropdown.show()
+  }
+  else{
+    $(this).removeClass('is-active')
+    dropdown.hide()
+  }
+});
+$('.toggle-button').click(function(e) {
+  e.preventDefault()
+  var menu = $(this).next()
+  if(menu.is(':hidden')){
+    menu.show()
+  }
+  else{
+    menu.hide()
+  }
 });
